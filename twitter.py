17c9968244aae966090ed506b79edf2688805a11
Akshit Bansal <akshitbansal@hotmail.com>
2018-02-25 14:56:07 -0500

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -5,11 +5,11 @@ wiki =TextBlob("He is angry because he did not get good food")
 print(wiki.tags)
 print(wiki.sentiment.polarity)
 
-consumer_key="fK6TPEPTC1qaYQw5eWyJk4vih"
-consumer_secret="UWLv3fwcIQ3ibsHyrbUtmoms5ixVYw3wjXOtS4r0xhUMdGCc3R"
+consumer_key="#"
+consumer_secret="#"
 
-access_token="967783932077932545-UuLfoUfFQIFp3ikwRhtK8h7lDdAZh8W"
-access_token_secret="dEBs7c6aaXKC6iGx5V2pzmKUyuOkhqEwQdT42prfuILAS"
+access_token="#"
+access_token_secret="#"
 
 auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
