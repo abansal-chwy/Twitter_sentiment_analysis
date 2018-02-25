769a22445a4419d83bb03ed256782537903a63e9
Akshit Bansal <akshitbansal@hotmail.com>
2018-02-25 16:30:32 -0500

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -1,5 +1,8 @@
 from  textblob import TextBlob
 import tweepy
+import csv
+
+import pandas as pd
 
 wiki =TextBlob("He is angry because he did not get good food")
 print(wiki.tags)
@@ -14,9 +17,23 @@ access_token_secret="#"
 auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
 
-api=tweepy.API(auth)
-public_tweets=api.search("Rahul Gandhi")
-for tweet in public_tweets:
-    print(tweet.text)
-    analysis=TextBlob(tweet.text)
-    print(analysis.sentiment)
+
+
+with open('output.csv', 'w',encoding="utf-8", newline='\n') as  f:
+    writer = csv.DictWriter(f, fieldnames=['Tweet', 'Sentiment','Polarity'])
+    writer.writeheader()
+    api=tweepy.API(auth)
+    public_tweets=api.search("Trump")
+    for tweet in public_tweets:
+
+        #print(tweet.text)
+        analysis=TextBlob(tweet.text)
+        split=str(analysis.sentiment).split(",")  #break polarity and sentiment
+        sentiment=split[0].split("=")[1] # find the value of polarity
+
+        if(float(sentiment)>=0.0):        #check the value of polarity to classify
+           polarity="positive"
+        else:
+            polarity = "negative"
+        writer.writerow({'Tweet':tweet.text, 'Sentiment':sentiment,'Polarity':polarity})
+    #print(analysis.sentiment)
